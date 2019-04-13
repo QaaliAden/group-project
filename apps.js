4b8c9d010c86fa67a61ef7537ed0a50bca999d08
QaaliAden <46288896+QaaliAden@users.noreply.github.com>
2019-04-13 23:10:34 +0100

    Add files via upload

--- apps.js
+++ apps.js
@@ -1,4 +1,3 @@
-
 const navSlide= ()=> {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -24,30 +23,9 @@ const navSlide= ()=> {
     });
 }
 navSlide();
-
-const navSlide= ()=> {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks=document.querySelectorAll('.nav-links li');
-
- //Toggle Nav
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-
-  // Animation Links
-    navLinks.forEach((link, index) =>{
-      console.log(index);
-       if(link.style.animation) {
-           link.style.animation=''
-        } else {
-
-            link.style.animation=`navLinkFade 0.5s ease forwards ${index /7+0.5}s`;
-        }
-          });
-        // burger Animation
-        burger.classList.toggle(`toggle`);
-
-    });
-}
-navSlide();
-
+$(function () {
+         $(document).scroll(function () {
+             var $nav = $("#main");
+             $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+         });
+     });
